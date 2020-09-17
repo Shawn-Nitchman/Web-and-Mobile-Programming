a6d030bb4f6ce78aaa590a89f4661a530593de86
Shawn Nitchman <smng47@mail.umkc.edu>
2020-09-16 23:48:08 -0500

    Created github user

--- Web Programming/ICP4/Git Hub User Finder/script.js
+++ Web Programming/ICP4/Git Hub User Finder/script.js
@@ -1,15 +1,47 @@
 function getGithubInfo(user) {
     //1. Create an instance of XMLHttpRequest class and send a GET request using it.
     // The function should finally return the object(it now contains the response!)
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("Get", "https://api.github.com/users/"+ user, true);
+
+    xhttp.onload = function(){
+        if(xhttp.status === 200){
+            var data = JSON.parse(xhttp.responseText);
+            showUser(data);
+        }else{
+            noSuchUser(user);
+        }
+
+    }
+    xhttp.send();
 }
 
 function showUser(user) {
     //2. set the contents of the h2 and the two div elements in the div '#profile' with the user content
+
+    $('#profile h2').html("U/N: " + user["login"]);
+
+    var data = {"pic_url": user["avatar_url"]}
+    $("#profilePic").attr("src",data.pic_url);
+
+    document.getElementById("id").innerHTML = "ID: " + user["id"];
+
+    var str = "Link to account: ";
+    var result = str.link(user["html_url"],);
+    document.getElementById("link").innerHTML = result;
 }
 
 function noSuchUser(username) {
     //3. set the elements such that a suitable message is displayed
+    $('#profile h2').html("No user found try again.");
+
+    $("#profilePic").attr("src","");
+
+    $(".information").text("");
 }
+
+
 
 $(document).ready(function () {
     $(document).on('keypress', '#username', function (e) {
@@ -19,15 +51,8 @@ $(document).ready(function () {
             username = $(this).val();
             //reset the text typed in the input
             $(this).val("");
-            //get the user's information and store the respsonse
-            response = getGithubInfo(username);
-            //if the response is successful show the user's details
-            if (response.status == 200) {
-                showUser(JSON.parse(response.responseText));
-                //else display suitable message
-            } else {
-                noSuchUser(username);
-            }
+            //Goes to find the user
+            getGithubInfo(username);
         }
     })
 });
